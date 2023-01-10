53c41c1540aa3d82f2813fc732e811e4ab246d2b
rolteame <r.s.adebiyi@gmail.com>
2023-01-10 01:30:14 +0100

    feature: Sponsors section story completed

---
+++ src/components/Sponsors-Section/Sponsors.stories.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import {Sponsors} from './Sponsors'
+
+
+export default {
+  title: 'Sponsors-Secton/sponsors',
+  component: Sponsors
+}
+
+const Template = (args) => <Sponsors />
+
+export const Desktop = Template.bind({});
+
+export const sponsors = {}
